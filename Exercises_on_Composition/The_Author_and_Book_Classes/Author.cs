4626b250854066bf2c1e77b72a057bb50e48be97
NguyenHieu20468 <hieu.187pm20468@vanlanguni.vn>
2019-10-31 14:30:11 +0700

    Done

---
+++ Exercises_on_Composition/The_Author_and_Book_Classes/Author.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Author_and_Book_Classes
+{
+    public class Author
+    {
+        private string Name, Email;
+        private char Gender;
+        
+        public Author(string name, string email, char gender)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Gender = gender;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public string getEmail()
+        {
+            return this.Email;
+        }
+
+        public void setEmail(string email)
+        {
+            this.Email = email;
+        }
+
+        public char getGender()
+        {
+            return this.Gender;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Author[name={0}, email={1}, gender={2}", Name, Email, Gender);
+        }
+    }
+}
---
+++ Exercises_on_Composition/The_Author_and_Book_Classes/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Author_and_Book_Classes
+{
+    public class Book
+    {
+        private string Name;
+        private Author Author;
+        private double Price;
+        private int Qty =0;
+
+        public Book(string name, Author author, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Author = author;
+        }
+
+        public Book(string name, Author author, double price, int qty)
+            :this(name,author,price)
+        {
+            this.Qty = qty;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+        public Author getAuthor()
+        {
+            return this.Author;
+        }
+
+        public double getPrice()
+        {
+            return this.Price;
+        }
+
+        public void setPrice(double price)
+        {
+            this.Price = price;
+        }
+
+        public int getQty()
+        {
+            return this.Qty;
+        }
+
+        public void setQty(int qty)
+        {
+            this.Qty = qty;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Book [ name = {0}, Author [{1}], price = {2}, qty = {3} ", Name, Author, Price, Qty);
+        }
+
+        public string getAuthorName()
+        {
+            return this.Author.getName();
+        }
+
+        public string getAuthorEmail()
+        {
+            return this.Author.getEmail();
+        }
+
+        public string getAuthorGender()
+        {
+            return this.Author.getGender();
+        }
+    }
+}
